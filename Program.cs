33aeefbf98ac392b787863afceb1aa7d567a23bc
adamishakad <155333634+adamishakad@users.noreply.github.com>
2024-01-19 09:53:39 -0800

    Updated Program File

--- Program.cs
+++ Program.cs
@@ -4,8 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your name?");
 
+            // Reading user input
+            string userName = Console.ReadLine();
+
+
+            // Responding to user input
+            Console.WriteLine("Hello, " + userName + "!");
+
+            Console.WriteLine("What is your favorite color?");
+            string favoriteColor = Console.ReadLine();
+
+            // Responding with the favorite color
+            Console.WriteLine("Ah, " + favoriteColor + " is a great color!");
+
+            Console.WriteLine(userName + ", who likes " + favoriteColor + ", welcome to the program!");
         }
     }
 }
