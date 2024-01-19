5216257cbeeba2355f2a187a1deac7a3f17cb46d
adamishakad <155333634+adamishakad@users.noreply.github.com>
2024-01-19 09:44:31 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleReadLine2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+        }
+    }
+}
